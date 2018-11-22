ad9fc76674ab5132cc7d7c3e2bcdfd4a703bced4
Isadora Weber <20151inf0093@ad.spg.ifsul.edu.br>
2018-11-22 07:53:17 -0200

    Ultima versao

--- venv/interface.py
+++ venv/interface.py
@@ -40,86 +40,28 @@ class interface():
     def main(self):
 
         janela = tk.Tk()
-        janela.geometry("1024x500")
+        janela.geometry("900x700")
         janela.title("Agenda")
         cor = StringVar(janela)
         cor.set("white")
         l = Label(background=cor.get())
         l.pack(fill='both', expand=True)
 
-        lb = Label(janela, text="AGENDA", fg="magenta", font=("Verdana", 16, "bold"), bg="white").place(x=450, y=10)
+        lb = Label(janela, text="AGENDA", fg="magenta", font=("Verdana", 16, "bold"), bg="white").place(x=430, y=10)
 
-        lb = Label(janela, text="Horário", fg="black", font=("Verdana", 12, "bold"), bg="white").place(x=105, y=70)
+        lb = Label(janela, text="Horário:", fg="black", font=("Verdana", 12, "bold"), bg="white").place(x=300, y=100)
+        horario = Entry(janela, fg="black", font=("Verdana", 12), bg="white", width=8)
+        horario.place(x=400, y=100)
 
-        # horario = Entry(janela, fg="black", font=("Verdana", 12), bg="white", width=8)
-        # horario.place(x=100, y=100)
-        #
-        # horario = Entry(janela, fg="black", font=("Verdana", 12), bg="white", width=8)
-        # horario.place(x=100, y=140)
-        #
-        # horario = Entry(janela, fg="black", font=("Verdana", 12), bg="white", width=8)
-        # horario.place(x=100, y=180)
-        #
-        # horario = Entry(janela, fg="black", font=("Verdana", 12), bg="white", width=8)
-        # horario.place(x=100, y=220)
-        #
-        # horario5 = Entry(janela, fg="black", font=("Verdana", 12), bg="white", width=8)
-        # horario5.place(x=100, y=260)
-        #
-        # horario6 = Entry(janela, fg="black", font=("Verdana", 12), bg="white", width=8)
-        # horario6.place(x=100, y=300)
+        lb = Label(janela, text="Disciplina:", fg="black", font=("Verdana", 12, "bold"), bg="white").place(x=300, y=150)
+        disciplina = Entry(janela, fg="black", font=("Verdana", 12), bg="white", width=25)
+        disciplina.place(x=400, y=150)
 
-        h = []
-
-
-        seg = []
-        ter = []
-        qua = []
-        qui = []
-        sex = []
-
-        for x in range(0,6):
-
-            h.insert(x, Entry(janela, fg="black", font=("Verdana", 12), bg="white", width=8))
-            h[x].place(x = 100, y = 100+(40*x))
-
-            seg.insert(x, Entry(janela, fg="black", font=("Verdana", 12), bg="white", width=12))
-            seg[x].place(x = 200, y = 100+(40*x))
-
-            ter.insert(x, Entry(janela, fg="black", font=("Verdana", 12), bg="white", width=12))
-            ter[x].place(x = 350, y = 100+(40*x))
-
-            qua.insert(x, Entry(janela, fg="black", font=("Verdana", 12), bg="white", width=12))
-            qua[x].place(x = 500, y = 100+(40*x))
-
-            qui.insert(x, Entry(janela, fg="black", font=("Verdana", 12), bg="white", width=12))
-            qui[x].place(x = 650, y = 100+(40*x))
-
-            sex.insert(x, Entry(janela, fg="black", font=("Verdana", 12), bg="white", width=12))
-            sex[x].place(x = 800, y = 100+(40*x))
-
-        # SEGUNDA
-
-        lb = Label(janela, text="Segunda", fg="black", font=("Verdana", 12, "bold"), bg="white").place(x=220, y=70)
-
-        # TERÇA
-        lb = Label(janela, text="Terça", fg="black", font=("Verdana", 12, "bold"), bg="white").place(x=385, y=70)
-
-
-        # QUARTA
-        lb = Label(janela, text="Quarta", fg="black", font=("Verdana", 12, "bold"), bg="white").place(x=530, y=70)
-
-
-        # QUINTA
-        lb = Label(janela, text="Quinta", fg="black", font=("Verdana", 12, "bold"), bg="white").place(x=680, y=70)
-
-        #SEXTA
-        lb = Label(janela, text="Sexta", fg="black", font=("Verdana", 12, "bold"), bg="white").place(x=830, y=70)
-
-
+        lb = Label(janela, text="Dia da semana:", fg="black", font=("Verdana", 12, "bold"), bg="white").place(x=300, y=200)
+        rb1 = Radiobutton(janela, text="Segunda-feira", fg="black", font=("Verdana", 12), bg="white", width=25)
 
         bt=Button(janela, text="SALVAR", fg="white", font=("Verdana", 12, "bold"), bg="magenta", command = lambda: self.cadastarAgenda(h, seg,ter,qua, qui, sex))
-        bt.place(x=835, y=350)
+        bt.place(x=430, y=500)
 
         # self.cadastrarAgenda(h, seg, ter, qua, qui, sex)
 
